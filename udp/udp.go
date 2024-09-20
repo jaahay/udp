b23cf17614dfd2f5d1b6b5282051d1d18dad0f50
jaahay <jaahay@users.noreply.github.com>
2024-09-20 16:26:26 -0700

    little more packaging

--- udp/udp.go
+++ udp/udp.go
@@ -5,7 +5,9 @@ import (
 	"net"
 )
 
-type Config struct {
+type Server interface {
+	GetOrMakeClient(addr net.Addr) client
+	Send(s string, client client)
 }
 
 // net.PacketConn
@@ -26,55 +28,49 @@ type Conn interface {
 // 	String() string
 // }
 
-type Server struct {
+type server struct {
 	conn           Conn
-	clients        map[net.Addr]Client
-	clientSessions map[Client]ClientSession
+	clients        map[net.Addr]client
+	clientSessions map[client]clientSession
 }
 
-type Client struct {
+type client struct {
 	addr net.Addr
 }
 
-type ClientSession struct {
-	clientConnection ClientConnection
-	// session          func()
+type clientSession struct {
+	clientConnection clientConnection
 }
 
-type ClientConnection struct {
+type clientConnection struct {
 	addr net.Addr
 }
 
 func NewServer(conn Conn) Server {
-	return Server{
+	return server{
 		conn,
-		make(map[net.Addr]Client),
-		make(map[Client]ClientSession),
+		make(map[net.Addr]client),
+		make(map[client]clientSession),
 	}
 }
 
-func (server *Server) GetOrMakeClient(addr net.Addr) Client {
-	// todo: authentication, then;
-	client, ok := server.clients[addr]
+func (server server) GetOrMakeClient(addr net.Addr) client {
+	c, ok := server.clients[addr]
 	if !ok {
-		client := Client{addr}
-		server.clients[addr] = client
+		c := client{addr}
+		server.clients[addr] = c
 	}
-	_, ok = server.clientSessions[client]
+	_, ok = server.clientSessions[c]
 	if !ok {
-		clientConnection := ClientConnection{addr}
-		// todo: client-based sessions; not Addr-based
-		udpSession := ClientSession{
-			clientConnection: clientConnection,
-			// session:          go func (),
-		}
-		server.clientSessions[client] = udpSession
-		return client
+		clientConnection := clientConnection{addr}
+		udpSession := clientSession{clientConnection}
+		server.clientSessions[c] = udpSession
+		return c
 	}
-	return client
+	return c
 }
 
-func (server *Server) Send(s string, client Client) {
+func (server server) Send(s string, client client) {
 	session := server.clientSessions[client]
 	server.conn.WriteTo([]byte(s), session.clientConnection.addr)
 	fmt.Println(s)
